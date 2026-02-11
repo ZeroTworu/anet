dc26cbe0cf08f4f88bf4aaab9890841f5bf557b6
Alpha <ao.200390@gmail.com>
2026-02-12 00:49:36 +0300

    fixed

---
+++ quick_start/i-am-so-lazy.sh
@@ -0,0 +1,32 @@
+echo "|-----------------------------------------------------|"
+echo "|   YOURE BITCH IS SO LAZY TO DO THIS SHIT YOURSELF   |"
+echo "|-----------------------------------------------------|"
+# ensure we are in the correct directory if install.sh, generate-config.sh, generate-client-config.sh, diagnose.sh, test-keys-from-keys-file.sh, test-client-keys.sh are in the same directory
+
+LINK_TO_ZIP="https://github.com/AlphaO612/easy_anet/releases/download/v1.0/i-am-so-lazy.zip"
+
+if [ ! -f install.sh ] || [ ! -f generate-config.sh ] || [ ! -f generate-client-config.sh ] || [ ! -f diagnose.sh ] || [ ! -f test-keys-from-keys-file.sh ] || [ ! -f test-client-keys.sh ]; then
+    echo "Error: scripts are not in the correct directory"
+    echo "Moving to the correct directory..."
+    mkdir -p easy_anet
+    cd easy_anet
+    echo "Downloading scripts..."
+
+    wget $LINK_TO_ZIP -O i-am-so-lazy.zip
+    unzip i-am-so-lazy.zip
+    rm i-am-so-lazy.zip
+fi
+
+./install.sh
+./generate-config.sh --clients 2
+./generate-client-config.sh --server-address $(curl -s https://icanhazip.com):8443
+docker compose up -d
+./diagnose.sh
+./test-keys-from-keys-file.sh
+./test-client-keys.sh client-windows/client.toml
+
+echo "Done! "
+echo "Your server is running on port 8443/UDP"
+echo "--------------------------------"
+echo "CONFIG: ./server/server.toml"
+echo "CLIENT KEYS: ./server/client-keys.txt"
