09c7ab9954c27168f5ab537dd0a46a0eca67bb35
lisenblsh <lisenblsh>
2026-05-11 23:08:16 +0700

    - remove all static_ip from auth and webui

--- anet-auth/src/migration/mod.rs
+++ anet-auth/src/migration/mod.rs
@@ -2,7 +2,6 @@ pub mod m20240101_000001_create_users;
 pub mod m20240319_000002_create_admins;
 pub mod m20240401_000003_create_rates;
 pub mod m20240501_000004_create_active_sessions;
-pub mod m20260504_152844_add_ipv4_to_user;
 
 use sea_orm_migration::prelude::*;
 
@@ -16,7 +15,6 @@ impl MigratorTrait for Migrator {
             Box::new(m20240319_000002_create_admins::Migration),
             Box::new(m20240401_000003_create_rates::Migration),
             Box::new(m20240501_000004_create_active_sessions::Migration),
-            Box::new(m20260504_152844_add_ipv4_to_user::Migration),
         ]
     }
 }
